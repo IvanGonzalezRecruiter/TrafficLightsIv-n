0dfb4ebbf0090e5d927cd6f3faaef366689c809f
IvanGonzalezRecruiter <ivan.gonzalez25@gmail.com>
2022-05-19 01:49:02 +0000

    .

---
+++ src/js/component/light.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export function Light(props) {
+	return (
+		<button
+			className={"light rounded-circle"}
+			style={{
+				backgroundColor:
+					props.color === props.glow ? props.color : "grey",
+			}}
+			onClick={() => props.setGlow(props.color)}
+		/>
+	);
+}
+
+Light.propTypes = {
+	color: PropTypes.string,
+	glow: PropTypes.string,
+	setGlow: PropTypes.func,
+};
+
+export default Light;
